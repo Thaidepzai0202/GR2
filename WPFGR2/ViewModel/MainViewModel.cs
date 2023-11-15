65df0aa9234bd9f9badd50a6f9f01c34e41ec196
Thaidepzai0202 <nguyenthethai02022001@gmail.com>
2023-11-16 01:43:34 +0700

    first

---
+++ WPFGR2/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFGR2.ViewModel
+{
+    public class MainViewModel 
+    {
+        public bool IsLoaded =false;
+        //all
+        public MainViewModel() {
+            if (!IsLoaded)
+            {
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+        }
+    }
+}
